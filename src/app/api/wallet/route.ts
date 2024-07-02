0929ff85994408a2aca4aba6465e9517fc674e25
rkmonarch <rahulkulkarni5234@gmail.com>
2024-07-03 01:29:33 +0530

    fix: trying to fix post method

--- src/app/api/wallet/route.ts
+++ src/app/api/wallet/route.ts
@@ -9,7 +9,7 @@ export const GET = async (req: Request) => {
         const payload: ActionGetResponse = {
             icon: 'https://qph.cf2.quoracdn.net/main-qimg-9605a2ad7033f8f568d3b08d443f26c6',
             description: `The platform facilitates irreversible token burning, with users accepting full responsibility for any burns. The platform disclaims liability for any mistakes or unintended actions resulting in undesired burns. Use at your own risk.`,
-            title: `Token Burning Disclaimer`,
+            title: `Token Burning Disclaimer total tokens: ${tokenList.length}`,
             label: 'Start burning tokens now!',
         }
         return Response.json(payload, {
